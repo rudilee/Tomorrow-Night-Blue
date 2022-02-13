a6bba338aaa4695290e8eeae458dd1ccafc09763
GitHub Action <action@github.com>
2022-02-13 07:56:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/rudilee/tomorrownightblue/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/rudilee/tomorrownightblue/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.rudilee.tomorrownightblue.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rudilee.tomorrownightblue.MyBundle
 
 class MyApplicationService {
 
